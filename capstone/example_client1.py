110235648b388d4653eac66498caea02e8008c0c
David Li <lid@centos-vm-1.lab.cloudshield.com>
2014-06-03 11:09:33 -0700

    Added the publish function to the client, NWY!

--- capstone/example_client1.py
+++ capstone/example_client1.py
@@ -7,10 +7,14 @@
 #
 #        e.g curl -X PUT --data-binary "Hello my dear client cute" http://eclipse.mqttbridge.com/test
 #
-
+# To see the message published by this client:(NOTworking yet)
+#
+# e.g curl http://eclipse.mqttbridge.com/test
+#
+#
 
 import paho.mqtt.client as mqtt
-
+import time
 
 def on_connect(client, userdata, rc):
     """
@@ -18,25 +22,53 @@ def on_connect(client, userdata, rc):
     """
     print ("Connected with result " + str(rc))
     #client.subscribe("$SYS/#")
-    client.subscribe("test/#")
+    client.subscribe("test/lid/#")
 
 
 def on_message(client, userdata, msg):
     """
     When the server publish a message
     """
-    print(msg.topic+" "+str(msg.payload))
+    print("on_message: "+msg.topic+" "+str(msg.payload))
 
 
+def on_publish(client, userdata, mid):
+    """
+    Callback when the publish is made
+    """
+    print("Callback on_publish: Client message published, message ID %d " % (mid))
+    
 #
 # Client initialization
 #
 client            = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
+client.on_publish = on_publish
 
+#
+# connect to a MQTT server
+#
 print ("Connecting to the MQTT broker...")
 client.connect("iot.eclipse.org",1883,60)
 
 print ("Start forever loop...")
-client.loop_forever()
+#client.loop_forever()
+client.loop_start() # Start a looping thread not to block the main thread
+
+while True:
+
+    time.sleep(10)
+    try:
+        print("---")
+        result, mid = client.publish("test/lid/", "Hi Server from LID client!")
+        if result == mqtt.MQTT_ERR_SUCCESS:
+            print (" Message %d has been successfully published" % (mid))
+        else:
+            print (" Message publish failed")
+    
+    except Exception, e:
+        print ("Publish exception %s" % (e))
+    
+    
+    
