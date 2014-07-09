cdb18c6882d087945ecc5cc22b282eb5c031112d
David Li <lid@centos-vm-1.lab.cloudshield.com>
2014-07-09 16:10:50 -0700

    This example paho python client is now working with Ponte mqtt-http bridge on publish!

--- capstone/example_client1.py
+++ capstone/example_client1.py
@@ -5,16 +5,21 @@
 # Usage: launch this client, find another machine and use the following
 #        publish a message to this client:
 #
-#        e.g curl -X PUT --data-binary "Hello my dear client cute" http://eclipse.mqttbridge.com/test
+#        e.g curl -X PUT --data-binary "Hello my client LID" http://eclipse.mqttbridge.com/test/lid
 #
 # To see the message published by this client:(NOTworking yet)
 #
-# e.g curl http://eclipse.mqttbridge.com/test
+# e.g curl http://eclipse.mqttbridge.com/test/lid
 #
 #
 
 import paho.mqtt.client as mqtt
 import time
+
+MOSQUITTO_SERVER = "10.10.36.87"
+MOSQUITTO_SERVER_PORT = 1884 
+
+PUB_TOPIC="hello-from-lid/test"
 
 def on_connect(client, userdata, rc):
     """
@@ -49,11 +54,11 @@ client.on_publish = on_publish
 #
 # connect to a MQTT server
 #
-print ("Connecting to the MQTT broker...")
-client.connect("iot.eclipse.org",1883,60)
+print ("Connecting to the MQTT broker: %s on port: %s" %( MOSQUITTO_SERVER, MOSQUITTO_SERVER_PORT))
+#client.connect("iot.eclipse.org",1883,60)
+client.connect(MOSQUITTO_SERVER,MOSQUITTO_SERVER_PORT,60)
 
 print ("Start forever loop...")
-#client.loop_forever()
 client.loop_start() # Start a looping thread not to block the main thread
 
 while True:
@@ -61,9 +66,12 @@ while True:
     time.sleep(10)
     try:
         print("---")
-        result, mid = client.publish("test/lid/", "Hi Server from LID client!")
+        #result, mid = client.publish("test/lid/", "Hi Server from LID client!")
+        PUB_MSG=time.ctime()
+        result, mid = client.publish(PUB_TOPIC, PUB_MSG, 0, True)
         if result == mqtt.MQTT_ERR_SUCCESS:
-            print (" Message %d has been successfully published" % (mid))
+            print (" Message %d has been successfully published on topic: %s with msg: %s" %
+                   (mid, PUB_TOPIC, PUB_MSG))
         else:
             print (" Message publish failed")
     
