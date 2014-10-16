ce4939cf1ed8a8a9abe87dc5d8745d43b8469b5e
DavidLi <David.Li@CS535A.(none)>
2014-10-16 13:28:20 -0700

    Added insert into the table

--- test/sqlite_test.c
+++ test/sqlite_test.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
+#include <time.h>
 #include <sqlite3.h>
-
+#include <string.h>
 
 #define DB_NAME "test.db"
-
+#define TABLE_NAME "CS535_STUDENT_FALL14"
 
 static int callback(void *notused, int argc, char **argv, char **azColName)
 {
@@ -17,17 +18,19 @@ static int callback(void *notused, int argc, char **argv, char **azColName)
 }
 
 
-static int create_db_table(sqlite3 *db, char * sql)
+static int exec_sql_db(sqlite3 *db, char * sql)
 {
    	char * errmsg;
-
+	int rc;
+        
+	fprintf(stderr,"SQL statement: %s\n", sql);
 	if (sqlite3_exec(db, sql, callback, 0, &errmsg) != SQLITE_OK) {
-		fprintf(stderr, "%s SQL error %s\n", __FUNCTION__,errmsg);
+		fprintf(stderr, "%s SQL error: %s\n", __FUNCTION__,errmsg);
 		sqlite3_free(errmsg);
 		return -1;
 	}
         else {
-                fprintf(stderr, "Table created OK!\n");
+                fprintf(stderr, "SQL statement executed OK!\n");
         }
         return 0;
 
@@ -62,20 +65,32 @@ int main(int argc, char* argv[])
 {
 	sqlite3 *db;
         int rc;
-
-        char * sql_statement;
+        
+	// fake records
+	int id1 = 1234;
+	int id2 = 4556;
+        time_t now;
+		
+        char sql_statement[256];
 
         // open DB 
         if (open_db(&db, DB_NAME) < 0 ) {
                  fprintf(stderr, "open_db failed\n");
         }
 
-        /* create SQL statement */
-        sql_statement = "CREATE TABLE CS535_STUDENT_FALL14(" \
-                        "ID INT PRIMARY KEY NOT NULL," \
-                        "TIME REAL);";
+        /* create SQL table if the table doesn't exist in the db*/
+        sprintf(sql_statement, "CREATE TABLE IF NOT EXISTS " TABLE_NAME  "(ID INT PRIMARY KEY NOT NULL,TIME CHAR(128));");
       
-        create_db_table(db, sql_statement); 
+        if (exec_sql_db(db, sql_statement) < 0 ) {
+               return 0;
+        }
+        // Insert into the table
+        time(&now);
+        char* ins_time = ctime(&now);
+
+        memset(sql_statement, 0, 256);
+	sprintf(sql_statement, "INSERT INTO " TABLE_NAME " (ID, TIME)  VALUES ( %d , '%s' );", id1, ins_time ); 	
+        exec_sql_db(db, sql_statement);
 
 	// Close DB 
         close_db(db);
